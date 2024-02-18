4a54bc30ca065a4301a731b335abbb77004e1d4b
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-02-18 01:43:50 -0300

    ex6.py

---
+++ ex6.py
@@ -0,0 +1,13 @@
+'''
+Write a Python program that accepts a sequence of comma-separated numbers from the user and generates a list and a tuple of those numbers.
+Sample data : 3, 5, 7, 23
+Output :
+List : ['3', ' 5', ' 7', ' 23']
+Tuple : ('3', ' 5', ' 7', ' 23')
+'''
+
+data = str(input("Type your data: ")).split(', ')
+print("List: %s\nTuple: %s" %(data, tuple(data)))
+
+
+
