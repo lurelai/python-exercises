a0ba02c22da236fd0d947929449d42c141d27ea2
Lucas Ariel <github.lucas.ariel27@gmail.com>
2024-02-17 23:30:18 -0300

    ex2.py

---
+++ ex2.py
@@ -0,0 +1,6 @@
+'''
+ Write a Python program to find out what version of Python you are using.
+'''
+import os
+os.system('python3 --version') # Using the sys module is better then use the os module.
+
